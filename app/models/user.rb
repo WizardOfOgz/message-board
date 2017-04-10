ecaf4db80c2acc1ee74ab272e7e63e8098b47879
WizardOfOgz <andyogzewalla@gmail.com>
2017-04-10 09:31:19 -0500

    Rspec config and should matchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :description, length: { maximum: 4_096 }  # Do not allow the user to save HUGE amounts of data to a text column. This limit is completely artbitrary.
 end
