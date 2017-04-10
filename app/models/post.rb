acf887d7390e749564d5fa6948836d305cd7dc3d
WizardOfOgz <andyogzewalla@gmail.com>
2017-04-10 15:38:43 -0500

    Add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 127 }
+  validates :body, presence: true, length: { maximum: 100_000 }
+  validates :user, presence: true
 end
