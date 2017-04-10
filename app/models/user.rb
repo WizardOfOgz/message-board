a019b219aa481366a7fc50b938dfd6640475cf29
WizardOfOgz <andyogzewalla@gmail.com>
2017-04-10 17:05:09 -0500

    Add User#name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
   validates :description, length: { maximum: 4_096 }  # Do not allow the user to save HUGE amounts of data to a text column. This limit is completely artbitrary.
+
+  def name
+    [*first_name, *last_name].join(" ")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe ::User, type: :model do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_length_of(:description).is_at_most(4_096) }
+
+  describe "#name" do
+    it "concats first and last name" do
+      subject = described_class.new(first_name: "Jon", last_name: "Smith")
+      expect(subject.name).to eq("Jon Smith")
+    end
+
+    it "returns proper value when first_name is nil" do
+      subject = described_class.new(first_name: nil, last_name: "Smith")
+      expect(subject.name).to eq("Smith")
+    end
+
+    it "returns proper value when last_name is nil" do
+      subject = described_class.new(first_name: "Jon", last_name: nil)
+      expect(subject.name).to eq("Jon")
+    end
+  end
 end
