ac9023d60ad1947a42ea0bb7cb76287b5ddeed83
WizardOfOgz <andyogzewalla@gmail.com>
2017-04-10 16:17:47 -0500

    Add Post model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe ::Comment, type: :model do
+  it { should validate_presence_of(:post) }
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_most(100_000) }
 end
