c703c0448a0084564af33558faa327a5b98145b1
WizardOfOgz <andyogzewalla@gmail.com>
2017-04-11 22:18:10 -0500

    Send mail through Sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,18 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:             Rails.application.secrets.sendgrid_username,
+    password:              Rails.application.secrets.sendgrid_password,
+    domain:                Rails.application.secrets.sendgrid_domain,
+    address:               'smtp.sendgrid.net',
+    port:                  587,
+    authentication:        :plain,
+    enable_starttls_auto:  true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.sendgrid_domain }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:             Rails.application.secrets.sendgrid_username,
+    password:              Rails.application.secrets.sendgrid_password,
+    domain:                Rails.application.secrets.sendgrid_domain,
+    address:               'smtp.sendgrid.net',
+    port:                  587,
+    authentication:        :plain,
+    enable_starttls_auto:  true
+  }
 end
